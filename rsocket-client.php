f5b5a2bf149d2cfca720f8f271effc215dcc930e
linux_china <libing.chen@gmail.com>
2020-07-15 10:57:33 -0700

    polishing

--- rsocket-client.php
+++ rsocket-client.php
@@ -5,6 +5,7 @@ require 'vendor/autoload.php';
 use RSocket\Payload;
 use RSocket\RSocket;
 use RSocket\RSocketConnector;
+use Rx\Observable;
 use Rx\Scheduler;
 
 $loop = React\EventLoop\Factory::create();
@@ -14,16 +15,19 @@ Scheduler::setDefaultFactory(function () use ($loop) {
     return new Scheduler\EventLoopScheduler($loop);
 });
 
-$rsocketPromise = RSocketConnector::create($loop)->connect("tcp://127.0.0.1:42252");
-
-$rsocketPromise->then(function (RSocket $rsocket) {
+$rsocketCall = function (RSocket $rsocket) {
     $observablePayload = $rsocket->requestResponse(Payload::fromText("text/plain", "Ping"));
     $observablePayload->subscribe(
         function (Payload $x) {
             echo 'Result: ' . $x->getDataUtf8();
         }
     );
-});
+};
+
+$rsocketPromise = RSocketConnector::create($loop)->connect("tcp://127.0.0.1:42252");
+$rsocketPromise->then($rsocketCall);
+$target = Observable::fromPromise($rsocketPromise);
+$target->map($rsocketCall)->subscribe();
 
 $loop->run();
 
