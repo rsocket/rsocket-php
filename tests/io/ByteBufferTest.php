dd1754f5eda06923132439c6b4314da80352b8ff
linux_china <libing.chen@gmail.com>
2020-07-15 10:24:37 -0700

    ByteBufferTest

---
+++ tests/io/ByteBufferTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace io;
+
+use PHPUnit\Framework\TestCase;
+use RSocket\io\ByteBuffer;
+
+class ByteBufferTest extends TestCase
+{
+    public function testRead(): void
+    {
+        $array = array(1, 2, 1, 1);
+        $buffer = ByteBuffer::wrap($array);
+        print($buffer->readI8());
+        print($buffer->readI8());
+        print($buffer->readI16());
+    }
+
+    public function testWrite(): void
+    {
+        $buffer = new ByteBuffer();
+        $buffer->writeI8(1);
+        $buffer->writeI8(2);
+        print_r($buffer->toUint8Array());
+    }
+}
