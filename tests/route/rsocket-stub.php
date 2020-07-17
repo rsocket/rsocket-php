7588bc4638a9f7d8325e01db7e60cc6720c00e1e
linux_china <libing.chen@gmail.com>
2020-07-17 15:53:14 -0700

    rsocket stub

---
+++ tests/route/rsocket-stub.php
@@ -0,0 +1,55 @@
+<?php
+
+require '../../vendor/autoload.php';
+
+use RSocket\Payload;
+use RSocket\routing\RSocketServiceStub;
+use RSocket\RSocket;
+use RSocket\RSocketConnector;
+use Rx\Observable;
+use Rx\Scheduler;
+
+interface AccountService
+{
+    public function findById(string $id): Observable;
+}
+
+class RSocketAccountServiceStubs
+{
+    private Observable $rsocket;
+
+    public function __construct(Observable $rsocket)
+    {
+        $this->rsocket = $rsocket;
+    }
+
+    /**
+     * @return \AccountService user service
+     */
+    public function getAccountService(): object
+    {
+        return new RSocketServiceStub("com.example.AccountService", $this->rsocket);
+    }
+
+}
+
+$loop = React\EventLoop\Factory::create();
+
+/** @noinspection PhpUnhandledExceptionInspection */
+Scheduler::setDefaultFactory(function () use ($loop) {
+    return new Scheduler\EventLoopScheduler($loop);
+});
+
+$rsocket = Observable::fromPromise(RSocketConnector::create($loop)->connect("tcp://127.0.0.1:42252"));
+
+$stubs = new RSocketAccountServiceStubs($rsocket);
+
+$accountService = $stubs->getAccountService();
+
+$accountService->findById("1")->subscribe(
+    function (string $name) {
+        echo 'Result: ' . $name . PHP_EOL;
+    }
+);
+
+$loop->run();
