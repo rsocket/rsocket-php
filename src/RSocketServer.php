6cb61c1bd0e07116ec065d3b52ac3674923dc82c
linux_china <libing.chen@gmail.com>
2020-07-17 23:54:06 -0700

    add WebSocket support

--- src/RSocketServer.php
+++ src/RSocketServer.php
@@ -6,7 +6,9 @@ namespace RSocket;
 
 use React\EventLoop\LoopInterface;
 use  React\Socket\TcpServer;
-use RSocket\core\RSocketResponder;
+use RSocket\core\RatchetWebSocketRSocketResponder;
+use RSocket\core\RSocketException;
+use RSocket\core\TcpSocketRSocketResponder;
 use RSocket\io\Closeable;
 
 class RSocketServer
@@ -17,16 +19,27 @@ class RSocketServer
     public static function create(LoopInterface $loop, SocketAcceptor $socketAcceptor): RSocketServer
     {
         $server = new self();
-        $server->loop= $loop;
+        $server->loop = $loop;
         $server->socketAcceptor = $socketAcceptor;
         return $server;
     }
 
     public function bind(string $url): Closeable
     {
-        $server = new TcpServer($url, $this->loop);
-        $responder = new RSocketResponder($url, $server, $this->socketAcceptor, $this->loop);
-        $responder->accept();
-        return $responder;
+        $urlArray = parse_url($url);
+        if ($urlArray !== false && array_key_exists("scheme", $urlArray)) {
+            $scheme = $urlArray['scheme'];
+            if ($scheme === 'tcp') {
+                $server = new TcpServer($url, $this->loop);
+                $responder = new TcpSocketRSocketResponder($url, $server, $this->socketAcceptor, $this->loop);
+                $responder->accept();
+                return $responder;
+            }
+            if ($scheme === 'ws') {
+                $responder = new RatchetWebSocketRSocketResponder($url, $this->socketAcceptor, $this->loop);
+                return $responder;
+            }
+        }
+        throw new RSocketException("Failed to listen " . $url);
     }
 }
